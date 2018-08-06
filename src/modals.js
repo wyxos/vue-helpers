c5b88ce5552a77f2b449ad016ad87454fc65ba16
JoeyJ <github@wyxos.com>
2018-08-06 20:18:59 +0400

    - Initial commit

---
+++ src/modals.js
@@ -0,0 +1,16 @@
+export default {
+    install(Vue, options) {
+        Vue.component('modals', require('./components/Modals.vue'));
+
+        Vue.prototype.$modals = new Vue({
+            methods: {
+                open(modal, options) {
+                    this.$events.fire('openModal', modal, options);
+                },
+                close(modal, options) {
+                    this.$events.fire('closeModal', modal, options);
+                }
+            }
+        });
+    }
+};
