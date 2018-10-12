ee8c4da4bddd0ba41a80efbc21415de0979d82f3
jjoey <joey@orckid.com>
2018-10-12 09:20:27 +0400

    - Changed keen-ui dependency to point to next release branch

--- src/orckid.js
+++ src/orckid.js
@@ -4,32 +4,11 @@ import state from './state';
 import modals from './modals';
 import boolean from './boolean';
 import request from './request';
-import UiAlert from 'keen-ui/src/UiAlert';
-import UiButton from 'keen-ui/src/UiButton';
-import UiIconButton from 'keen-ui/src/UiIconButton';
-import UiConfirm from 'keen-ui/src/UiConfirm';
-import UiIcon from 'keen-ui/src/UiIcon';
-import UiModal from 'keen-ui/src/UiModal';
+import KeenUi from 'keen-ui';
 
 export default {
     install(Vue, options) {
-        Vue.component('ui-alert', UiAlert);
-        Vue.component('ui-button', UiButton);
-        Vue.component('ui-icon-button', UiIconButton);
-        Vue.component('ui-confirm', UiConfirm);
-        Vue.component('ui-icon', UiIcon);
-        Vue.component('ui-modal', {
-            mixins: [UiModal],
-            methods: {
-                restrictFocus(e) {
-                    // if (!this.$refs.container.contains(e.target)) {
-                    //   e.stopPropagation();
-                    //   this.$refs.container.focus();
-                    // }
-                }
-            }
-        });
-
+        Vue.use(KeenUi);
         Vue.use(events);
         Vue.use(errors);
         Vue.use(state);
