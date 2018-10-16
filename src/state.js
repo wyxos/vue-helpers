f76d0cceea08a8a6514d7b643abb3a28f62d618c
jjoey <github@wyxos.com>
2018-10-17 02:51:36 +0400

    - Optimized code format - Added exposure to global errors instance

--- src/state.js
+++ src/state.js
@@ -1,40 +1,40 @@
 export default {
-    install(Vue, options) {
-        Vue.prototype.$state = new Vue({
-            data() {
-                return {
-                    items: []
-                };
-            },
-            methods: {
-                running(name) {
-                    if (!name) {
-                        return Boolean(this.items.length);
-                    }
+  install(Vue, options) {
+    Vue.prototype.$state = new Vue({
+      data() {
+        return {
+          items: []
+        };
+      },
+      methods: {
+        running(name) {
+          if (!name) {
+            return Boolean(this.items.length);
+          }
 
-                    return Boolean(this.items.find(loader => loader.name === name));
-                },
-                add(name) {
-                    if (this.running(name)) {
-                        return;
-                    }
+          return Boolean(this.items.find(loader => loader.name === name));
+        },
+        add(name) {
+          if (this.running(name)) {
+            return;
+          }
 
-                    this.items.push({
-                        name
-                    });
-                },
-                clear(name) {
-                    if (!name) {
-                        this.items = [];
+          this.items.push({
+            name
+          });
+        },
+        clear(name) {
+          if (!name) {
+            this.items = [];
 
-                        return;
-                    }
+            return;
+          }
 
-                    const match = this.items.findIndex(loader => loader.name === name);
+          const match = this.items.findIndex(loader => loader.name === name);
 
-                    this.items.splice(match, 1);
-                }
-            }
-        });
-    }
+          this.items.splice(match, 1);
+        }
+      }
+    });
+  }
 };
