c1526c992d82236a08328a5f690b56c4661fef13
jjoey <joey@orckid.com>
2018-11-19 11:55:22 +0400

    - Initial commit of v1.1

--- src/orckid.js
+++ src/orckid.js
@@ -3,8 +3,8 @@ import events from 'vue-events';
 import state from './state';
 import modals from './modals';
 import boolean from './boolean';
-import request from './request';
 import KeenUi from 'keen-ui/dist/keen-ui.min';
+import axiosPlugin from './axios-plugin';
 
 export default {
   install(Vue, options) {
@@ -14,10 +14,14 @@ export default {
     Vue.use(state);
     Vue.use(modals);
     Vue.use(boolean);
-
-    Vue.prototype.$axios = request;
+    Vue.use(axiosPlugin);
 
     Vue.prototype.$routes = function (name, parameters) {
+      if (!window.Laravel) {
+        console.error('Laravel is not globally defined.');
+        return;
+      }
+
       return name ? window.Laravel.routes[name] : window.Laravel.routes;
     };
 
@@ -28,6 +32,37 @@ export default {
         },
         closeModal(ref) {
           this.$refs[ref].close();
+        },
+        handleError(error) {
+          const status = error && error.response ? error.response.status : 500;
+
+          const hasErrors = error.response && error.response.data && error.response.data.errors;
+
+          if (status === 403) {
+            throw error;
+          }
+
+          if (hasErrors && status === 422) {
+            this.$errors.setBag(error.response.data.errors);
+
+            this.openModal('formErrors');
+
+            throw error;
+          }
+
+          if ([401, 419].indexOf(status) > -1) {
+            throw error;
+          }
+
+          const errors = {
+            'unexpected_error': ['An unexpected error occurred']
+          };
+
+          this.$errors.setBag(errors);
+
+          this.openModal('formErrors');
+
+          throw error;
         }
       }
     });
