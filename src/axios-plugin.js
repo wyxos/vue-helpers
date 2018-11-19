f6e88e750e6b99465c42391cacf4e4cf6756724d
jjoey <github@wyxos.com>
2018-11-19 11:55:22 +0400

    - Initial commit of v1.1

---
+++ src/axios-plugin.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+export default {
+  install(Vue) {
+    axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+    let token = document.head.querySelector('meta[name="csrf-token"]');
+
+    if (token) {
+      axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+    } else {
+      console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+    }
+
+    Vue.prototype.$axios = function () {
+      axios.interceptors.request.use((config) => {
+        this.$errors.clear();
+
+        this.$state.add('ajax');
+
+        return config;
+      });
+
+      axios.interceptors.response.use((response) => {
+        this.$state.clear('ajax');
+        return response;
+      }, (error) => {
+        this.$state.clear('ajax');
+
+        this.handleError(error);
+
+        return Promise.reject(error);
+      });
+
+      return axios;
+    };
+  }
+};
