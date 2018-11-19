fca97bcdbcff2ba11c6ee7a2cf3d22d349df06ec
jjoey <joey@orckid.com>
2018-11-19 20:39:43 +0400

    - Fixed usage of import and require

--- src/index.js
+++ src/index.js
@@ -1,52 +1,11 @@
+import errors from './plugins/errors';
+import state from './plugins/state';
+import modals from './plugins/modals';
+import boolean from './plugins/boolean';
+import axios from './plugins/axios-plugin';
+
 const keenUi = require('keen-ui/dist/keen-ui.min');
 const events = require('vue-events');
-const errors = require('./plugins/errors');
-const state = require('./plugins/state');
-const modals = require('./plugins/modals');
-const boolean = require('./plugins/boolean');
-const axios = require('./plugins/axios-plugin');
-
-let mixin = {
-  methods: {
-    openModal(ref) {
-      this.$refs[ref].open();
-    },
-    closeModal(ref) {
-      this.$refs[ref].close();
-    },
-    handleError(error) {
-      const status = error && error.response ? error.response.status : 500;
-
-      const hasErrors = error.response && error.response.data && error.response.data.errors;
-
-      if (status === 403) {
-        throw error;
-      }
-
-      if (hasErrors && status === 422) {
-        this.$errors.setBag(error.response.data.errors);
-
-        this.openModal('formErrors');
-
-        throw error;
-      }
-
-      if ([401, 419].indexOf(status) > -1) {
-        throw error;
-      }
-
-      const errors = {
-        'unexpected_error': ['An unexpected error occurred']
-      };
-
-      this.$errors.setBag(errors);
-
-      this.openModal('formErrors');
-
-      throw error;
-    }
-  }
-};
 
 export default {
   install(Vue, options) {
@@ -58,7 +17,47 @@ export default {
     Vue.use(boolean);
     Vue.use(axios);
 
-    Vue.mixin(mixin);
+    Vue.mixin({
+      methods: {
+        openModal(ref) {
+          this.$refs[ref].open();
+        },
+        closeModal(ref) {
+          this.$refs[ref].close();
+        },
+        handleError(error) {
+          const status = error && error.response ? error.response.status : 500;
+
+          const hasErrors = error.response && error.response.data && error.response.data.errors;
+
+          if (status === 403) {
+            throw error;
+          }
+
+          if (hasErrors && status === 422) {
+            this.$errors.setBag(error.response.data.errors);
+
+            this.openModal('formErrors');
+
+            throw error;
+          }
+
+          if ([401, 419].indexOf(status) > -1) {
+            throw error;
+          }
+
+          const errors = {
+            'unexpected_error': ['An unexpected error occurred']
+          };
+
+          this.$errors.setBag(errors);
+
+          this.openModal('formErrors');
+
+          throw error;
+        }
+      }
+    });
 
     Vue.prototype.$routes = function (name) {
       if (!window.Laravel) {
