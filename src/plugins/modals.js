bda61584182591785eaa5ddef7eb5b3a5a6d41c6
jjoey <joey@orckid.com>
2018-12-06 16:24:45 +0400

    - Reworked concept of global modals

--- src/plugins/modals.js
+++ src/plugins/modals.js
@@ -10,25 +10,5 @@ export default {
         }
       }
     });
-
-    Vue.mixin({
-      mounted() {
-        this.$events.on('openModal', (modal) => {
-          let match = this.$refs[modal];
-          if (!match) {
-            return;
-          }
-          match.open();
-        });
-
-        this.$events.on('closeModal', (modal) => {
-          let match = this.$refs[modal];
-          if (!match) {
-            return;
-          }
-          match.close();
-        })
-      }
-    })
   }
 };
