f97b6847794b268ce5b99f6af0e3ee7c958af530
jjoey <joey@orckid.com>
2019-01-22 21:51:41 +0400

    - Compiled

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,10 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
-  mode: 'development',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    libraryTarget: 'window',
-    library: 'VueHelpers'
+    filename: 'index.js'
   },
   module: {
     rules: [
