292bdb64b696c6c402e1024101a3d160212b5c9a
jjoey <github@wyxos.com>
2019-10-29 14:04:50 +0400

    - Added basic html to demo functionalities - Reverted modal helper method names - Altered error handler

---
+++ src/components/Example.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <ui-modal ref="exampleModal">
+            <button @click="closeModal('exampleModal')">Close</button>
+        </ui-modal>
+        <button @click="openModal('exampleModal')">
+            Open
+        </button>
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
