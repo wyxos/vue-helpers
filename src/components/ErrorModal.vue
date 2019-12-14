2163d5b2c90f2ad04b7e93c4675dc4ee984cef06
joeyj <joey.jaulimsing@live.com>
2019-12-15 02:58:48 +0400

    - Renamed $routes to $route - Added default modal to render errors - Added computed for errors

---
+++ src/components/ErrorModal.vue
@@ -0,0 +1,23 @@
+<template>
+    <ui-modal ref="errorModal"
+              v-cloak title="Error">
+        <div class="text-center">
+            <p v-if="$errors.isUnderMaintenance">The site is currently in maintenance. Try again in a moment.</p>
+            <p v-if="$errors.isUnauthorized">This action is not allowed.</p>
+            <p v-if="$errors.isUnexpected">An unexpected error occurred.</p>
+            <p v-if="$errors.isExpired">The form has expired. Please reload the page and try again.</p>
+            <p v-if="$errors.isInvalid">There is an error within the form or fields required for this action. <br>Please
+                review.</p>
+            <button class="btn btn-primary mt-4" type="button"
+                    @click="closeModal('errorModal')">Close
+            </button>
+        </div>
+    </ui-modal>
+
+</template>
+
+<script>
+    export default {
+        name: "ErrorModal"
+    }
+</script>
