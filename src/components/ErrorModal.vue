9fa4be5e0d4d8f61a60d187dd12025c6c7493ac8
joeyj <github@wyxos.com>
2020-01-06 01:25:56 +0400

    - Fixed error modal title

--- src/components/ErrorModal.vue
+++ src/components/ErrorModal.vue
@@ -2,13 +2,19 @@
     <ui-modal ref="errorModal"
               v-cloak :title="title">
         <div class="text-center">
-            <slot v-if="$errors.isUnderMaintenance" name="maintenance">The site is currently in maintenance. Try again in a moment.</slot>
+            <slot v-if="$errors.isUnderMaintenance" name="maintenance">The site is currently in maintenance. Try again
+                in a moment.
+            </slot>
             <slot v-if="$errors.isUnauthorized" name="unauthorized">This action is not allowed.</slot>
             <slot v-if="$errors.isUnexpected" name="unexpected">An unexpected error occurred.</slot>
             <slot v-if="$errors.isNotFound" name="not-found">This action does not exist.</slot>
-            <slot v-if="$errors.isUnauthenticated" name="unauthenticated">You need to be logged in to perform this action.</slot>
-            <slot v-if="$errors.isExpired" name="expired">The form has expired. Please reload the page and try again.</slot>
-            <slot v-if="$errors.isInvalid" name="invalid">One or more fields are invalid. Please review and try again.</slot>
+            <slot v-if="$errors.isUnauthenticated" name="unauthenticated">You need to be logged in to perform this
+                action.
+            </slot>
+            <slot v-if="$errors.isExpired" name="expired">The form has expired. Please reload the page and try again.
+            </slot>
+            <slot v-if="$errors.isInvalid" name="invalid">One or more fields are invalid. Please review and try again.
+            </slot>
             <div>
                 <slot name="actions">
                     <button class="btn btn-danger mt-4"
@@ -24,7 +30,9 @@
     export default {
         name: "ErrorModal",
         props: {
-            title: 'Error'
+            title: {
+                default: 'Error'
+            }
         }
     }
 </script>
