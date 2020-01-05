3f140cc3e3b0bb77d0dcc23fb1263a026e1af44d
joeyj <github@wyxos.com>
2020-01-06 00:41:38 +0400

    - Updated publish script

--- publish.bat
+++ publish.bat
@@ -1,1 +1,1 @@
-npm run prod & git add dist/* & git commit -m "Compiled" & git push & npm version %1 & npm publish
+npm run prod & git add dist/* & git commit -m "Compiled" & git push origin/master & npm version %1 & npm publish
