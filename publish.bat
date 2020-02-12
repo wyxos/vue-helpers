eb671ec9ce980d1fab4c77bdd4c2469948f6f287
joeyj <maverick.slayer@live.com>
2020-02-12 23:39:35 +0400

    - Fixed version - Fixed cookie delete method

--- publish.bat
+++ publish.bat
@@ -1,1 +1,1 @@
-npm run prod & git add dist/* & git commit -m "Compiled" & git push & npm version %1 & npm publish
+npm run prod & git add dist/* & git commit -m "Release" & git push & npm version %1 & npm publish
