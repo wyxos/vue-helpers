18a2f3e8a240289195453bcda7b2b33e7f1a5163
joeyj <github@wyxos.com>
2020-05-19 16:25:09 +0400

    - Removed keen-ui - Added modal component - Updated demo

---
+++ src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="show">
+        <slot>Modal content</slot>
+    </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        show: false
+      }
+    },
+    methods: {
+      open () {
+        this.show = true
+      },
+      close () {
+        this.show = false
+      }
+    }
+  }
+</script>
